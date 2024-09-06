0e3771598ecdd4633c3a89a58ddac60b51bcf612
DUGET <sporovd@gmail.com>
2024-09-06 21:31:22 +0300

    Movement trouble

---
+++ Assets/_Scripts/PlayerController.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private Rigidbody  rb;
+    [SerializeField] private float speed = 5;
+    [SerializeField] private float turnSpeed = 360;
+    private Vector3 _input;
+
+    private void Update()
+    {
+        GatherInput();
+        // Look();
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void GatherInput()
+    {
+        _input = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        if(Input.GetMouseButton(0)) Debug.Log("W");
+        // if(_input.x != 0 || _input.y != 0) Debug.Log(_input);
+    }
+
+    private void Look()
+    {
+        if (_input != Vector3.zero)
+        {
+            var relative = (transform.position + _input) - transform.position;
+            var rot = Quaternion.LookRotation(relative, Vector3.up);
+
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, rot, turnSpeed * Time.deltaTime);
+        }
+    }
+
+    private void Move()
+    {
+        // rb.MovePosition(transform.position + transform.forward * (_input.magnitude * (speed * Time.deltaTime)));
+        transform.position += _input;
+    }
+}
