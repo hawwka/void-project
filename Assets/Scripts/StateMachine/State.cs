6403ad94d2e9531928d5ae710e187e51999954ac
hawwka <poiuminaj@gmail.com>
2024-10-22 01:06:59 +0400

    State machine upd

---
+++ Assets/Scripts/StateMachine/State.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class State : MonoBehaviour, IState
+{
+    public virtual void OnEnter()
+    {
+        
+    }
+
+    public virtual void Update()
+    {
+        
+    }
+
+    public virtual void FixedUpdate()
+    {
+        
+    }
+
+    public virtual void OnExit()
+    {
+        
+    }
+}
